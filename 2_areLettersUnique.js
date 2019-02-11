231371d1a52c3cea0260c7695ab79b7e0f311ee9
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-11 11:59:16 -0500

    Added 2_areLettersUnique.js

---
+++ 2_areLettersUnique.js
@@ -0,0 +1,49 @@
+/*
+    Write a function that accepts a String as its only parameter, and returns true if the string has only unique letters, taking letter case into account.
+
+    solution("No duplicates")              == true
+    solution("abcdefghijklmnopqrstuvwxyz") == true
+    solution("AaBbCc")                     == true
+    solution("Hello, world")               == false
+*/
+
+// loop
+function solution1(str) {
+    
+}
+
+// reduce
+function solution2(str) {
+    
+}
+
+// reduce short(one line)
+function solution2short(str) {
+
+}
+
+// no if, loop, reduce
+function solution3(str) {
+    
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge string for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution2short()
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
