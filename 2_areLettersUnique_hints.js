fbd2be9eebc58ac541013e78e1912d22bfb8fc62
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-11 12:32:22 -0500

    Added 2_areLettersUnique_hints.js

---
+++ 2_areLettersUnique_hints.js
@@ -0,0 +1,68 @@
+/*
+Write a function that accepts a String as its only parameter, and returns true if the string has only unique letters, taking letter case into account.
+
+    solution("No duplicates")               == true
+    solution1("abcdefghijklmnopqrstuvwxyz") == true
+    solution1("AaBbCc")                     == true
+    solution1("Hello, world")               == false
+*/
+
+// loop
+function solution1(str) {
+    // 1. create variable to hold unique letters
+    // 2. loop through str
+        // 3. if index of letter in unique string is equal to -1, add it to unique
+
+    // 4. return if str is equal to unique
+}
+
+// reduce
+function solution2(str) {
+    // 1. convert str to array
+    // 2. reduce array 
+        // 3. if index of letter in accumulator is equal to -1, add it to acc
+        // 4. return accumulator
+
+    // 5. convert array to string and compare it to str
+}
+
+// reduce one line
+function solution2short(str) {
+    // 1. convert str to array
+    // 2. reduce array 
+        // 3. if index of letter in accumulator is equal to -1, add it to acc
+        // 4. return accumulator
+
+    // 5. convert array to string and compare it to str
+}
+
+// use spesific data type
+function solution3(str) {
+    // 1. convert str to array
+    // 2. convert array to Set
+    // 3. convert set to array
+    // 4. convert array to string
+
+    // 5. compare result string to str
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge string for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution2short()
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
