19bb80677735af3ce0a3fffc58dd6525208b10b2
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-12 12:50:48 -0500

    Added 8_condenseWhitespace.js

---
+++ 8_condenseWhitespace.js
@@ -0,0 +1,32 @@
+/*
+    Write a function that returns a string with any consecutive spaces replaced with a single space.
+
+    solution("a   b   c") === "a b c"
+    solution("    a")     === " a"
+    solution("abc")       === "abc"
+*/
+
+// loop
+function solution1(str){
+}
+
+// regExp
+function solution2(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
