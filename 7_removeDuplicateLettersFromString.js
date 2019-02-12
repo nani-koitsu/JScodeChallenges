5ee64952f7185f3c3d27920499440d5697fd7572
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-12 12:48:55 -0500

    Added 7_removeDuplicateLettersFromString.js

---
+++ 7_removeDuplicateLettersFromString.js
@@ -0,0 +1,50 @@
+/*
+    Write a function that accepts a string as its input, and returns the same string just with duplicate letters removed.
+
+    solution("wombat")      === "wombat"
+    solution("hello")       === "helo"
+    solution("Mississippi") === "Misp"
+*/
+
+// Loop
+function solution1(str){
+}
+
+// reduce
+function solution2(str) {
+}
+
+// reduce (short)
+function solution3(str) {
+}
+
+// filter
+function solution4(str) {
+}
+
+// filter short
+function solution5(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution3()
+
+// test solution4()
+
+// test solution5()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
