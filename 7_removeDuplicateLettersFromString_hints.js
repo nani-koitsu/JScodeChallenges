0c8709e687673056736bea0862ebb3416148f033
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-12 14:18:28 -0500

    Added 7_removeDuplicateLettersFromString_hints.js

---
+++ 7_removeDuplicateLettersFromString_hints.js
@@ -0,0 +1,76 @@
+/*
+    Write a function that accepts a string as its input, and returns the same string just with duplicate letters removed.
+
+    solution("wombat")      === "wombat"
+    solution("hello")       === "helo"
+    solution("Mississippi") === "Misp"
+*/
+
+// Loop
+function solution1(str){
+    // 1. create variable to hold unique letters
+    // 2. loop through str
+        // 3. if index of letter in unique string is equal to -1, add this letter to unique string
+
+    // 4. return unique str
+}
+
+// reduce
+function solution2(str) {
+    // 1. convert str to array
+    // 2. reduce array 
+        // 3. if indexOf letter in accumulator is equal to -1, add letter to accumulator
+    
+    // 4.convert array to string and return it
+}
+
+// reduce (short)
+function solution3(str) {
+    // 1. convert str to array
+    // 2. reduce array 
+        // 3. if indexOf letter in accumulator is equal to -1, add letter to accumulator
+    
+    // 4.convert array to string and return it
+}
+
+// filter
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+function solution4(str) {
+    // 1. convert str to arrStr
+    // 2.filter arrStr using (letter, index, array)
+        // 3. return if indexOf letter in array is equal "index"
+    
+    // 4. join arrStr and return it 
+}
+
+// filter short
+function solution5(str) {
+    // 1. convert str to arrStr
+    // 2.filter arrStr using (letter, index, array)
+        // 3. return if indexOf letter in array is equal "index"
+    
+    // 4. join arrStr and return it
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution3()
+
+// test solution4()
+
+// test solution5()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
