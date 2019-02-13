b86be4512c761a55aab03d73f7b45cf603e9c666
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-13 10:35:59 -0500

    Added 9_findPangrams.js

---
+++ 9_findPangrams.js
@@ -0,0 +1,51 @@
+/*
+    Write a function that returns true if it is given a string that is an English pangram, ignoring letter case.
+
+    Tip: A pangram is a string that contains every letter of the alphabet at least once.
+
+    solution("The quick brown fox jumps over the lazy dog")  === true
+    solution("The quick brown fox jumped over the lazy dog") === false
+*/
+
+// loop
+function solution1(str){
+}
+
+// reduce
+function solution2(str) {
+}
+
+// reduce short
+function solution3(str) {
+}
+
+// filter + Set
+function solution4(str) {
+}
+
+// filter + Set short
+function solution5(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution3()
+
+// test solution4()
+
+// test solution5()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
