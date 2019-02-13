955007d45db4c94b60380e352d1829848e64ee1c
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-13 15:01:50 -0500

    Added 12_ThreeDifferentLetters.js

---
+++ 12_ThreeDifferentLetters.js
@@ -0,0 +1,28 @@
+/*
+    Write a function that accepts two strings, and returns true if they are identical in length but have no more than three different letters, taking case and string order into account.
+    
+    solution("Clamp", "Cramp")  == true
+    solution("Clamp", "Crams")  == true
+    solution("Clamp", "Grams")  == true
+    solution("Clamp", "Grans")  == false
+    solution("Clamp", "Clam")   == false
+    solution("clamp", "maple")  == false
+*/
+
+function solution1(str1, str2){
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
