44e63236d31bd0ce214314c499b79bf5c27d948e
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-13 14:06:36 -0500

    Added 11_vowelsAndConsonants.js

---
+++ 11_vowelsAndConsonants.js
@@ -0,0 +1,26 @@
+/*
+    Given a string in English, return a tuple containing the number of vowels and consonants.
+
+    Tip: Vowels are the letters, A, E, I, O, and U; consonants are the letters B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z.
+    
+    solution("JavaScript Coding Challenges") == [8, 18]
+    solution("Mississippi")                  == [4, 7]
+*/
+
+function solution1(str){
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
