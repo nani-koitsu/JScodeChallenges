3a58aa64456ca6cd8a6383ab5df1a11aa842bbd6
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-13 14:22:49 -0500

    Added 11_vowelsAndConsonants_hints.js

---
+++ 11_vowelsAndConsonants_hints.js
@@ -0,0 +1,37 @@
+/*
+    Given a string in English, return a tuple containing the number of vowels and consonants.
+
+    Tip: Vowels are the letters, A, E, I, O, and U; consonants are the letters B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Y, Z.
+    
+    solution("JavaScript Coding Challenges") == [8, 18]
+    solution("Mississippi")                  == [4, 7]
+*/
+
+function solution1(str){
+    // 1. create variable "lowerStr" from lowercased str
+    // 2. create variable "vowels" equal to "aeiou"
+    // 3. create variable "consonants" equal to "bcdfghjklmnpqrstvwxyz"
+    // 4. create variable vowelCount equal to 0
+    // 5. create variable consonantCount equal to 0
+
+    // 6. loop through lowerStr
+        // 7. if index of letter in vowels is not equal to -1 increase vowelCount by 1
+        // 8. if index of letter in consonants is not equal to -1 increase consonantCount by 1
+ 
+    // 9. return array of vowelCount and consonantCount
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
