2a18346446e67a023c6d1e0215e2f58e6ce4b394
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-12 19:19:22 -0500

    Added 3_isPalindrom.js

---
+++ 3_isPalindrom.js
@@ -0,0 +1,45 @@
+/*
+    Write a function that accepts a String as its only parameter, and returns true if the string reads the same when reversed, ignoring case.
+
+    solution("rotator") === true
+    solution("Rats live on no evil star") === true
+    solution("Never odd or even") === false
+    solution("Hello, world") === false
+*/
+
+// reverse
+function solution1(str) {
+    
+}
+
+// loop
+function solution2(str) {
+    
+}
+
+// loop that provides earliest exit
+function solution3(str) {
+    
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge string
+
+
+// test solution1()
+
+
+// test solution2()
+
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
