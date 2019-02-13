e440ec69b9ec76bbc19eaed04f42450fef66e43a
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-13 12:59:10 -0500

    Added 10_stringIsRotated_hints.js

---
+++ 10_stringIsRotated_hints.js
@@ -0,0 +1,33 @@
+/*
+    Write a function that accepts two strings, and returns true if one string is rotation of the other, taking letter case into account.
+
+    Tip: A string rotation is when you take a string, remove some letters from its end, then append them to the front. For example, “swift” rotated by two characters would be "ftswi".
+
+    solution("abcde", "eabcd")  === true
+    solution("abcde", "cdeab")  === true
+    solution("abcde", "abced")  === false
+    solution("abc", "a")        === false
+*/
+
+function solution1(str1, str2){
+    // 1. return false if length of str1 is not equal to length of str2
+
+    // 2. create variable from repeating str1 twice
+
+    // 3. return if str2 exists in combined string
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
