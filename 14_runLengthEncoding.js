c6c8ef9b16a0254961e8b725d51c8eb4a053dd49
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-14 15:03:54 -0500

    Added 14_runLengthEncoding.js

---
+++ 14_runLengthEncoding.js
@@ -0,0 +1,32 @@
+/*
+    Write a function that accepts a string as input, then returns how often each letter is repeated in a single run, taking case into account.
+
+    Tip: This approach is used in a simple lossless compression technique called run-length encoding.
+
+    solution("aabbcc")      == "a2b2c2";
+    solution("aaabaaabaaa") == "a3b1a3b1a3";
+    solution("aaAAaa")      == "a2A2a2";
+*/
+
+function solution1(str) {
+}
+
+function solution2(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
