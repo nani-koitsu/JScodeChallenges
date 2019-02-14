80c78dbe67d25dbbd4c9ea92f0d6a74d490e3830
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-14 14:10:45 -0500

    Added 13_FindLongestPrefix_hints.js

---
+++ 13_FindLongestPrefix_hints.js
@@ -0,0 +1,37 @@
+/*
+    Write a function that accepts a string of words with a similar prefix, separated by spaces, and returns the longest substring that prefixes all words.
+    
+    solution("swift switch swill swim") == "swi"
+    solution("flip flap flop")          == "fl"
+*/
+
+function solution1(str) {
+    // 1. create array of words from str
+    // 2. get first word from array
+    // 3. create variable currentPrefix equal to ""
+    // 4. create variable bestPrefix equal to ""
+
+    // 5. loop through first word
+        // 6. add letter to currentPrefix
+
+        // 7. run one more loop thriugh words array
+            // 8. if !word.startsWith currentPrefix, return bestPrefix
+        
+        // 9. set bestPrefix to be equal to currentPrefix
+
+    // 10. return bestPrefix;
+}
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
