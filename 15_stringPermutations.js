dbd73695cd45f96fd41ef9530536b5d50b707902
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-19 13:47:14 -0500

    Added 15_stringPermutations.js

---
+++ 15_stringPermutations.js
@@ -0,0 +1,28 @@
+/*
+    Difficulty: Taxing
+
+    Write a function that returns all possible permutations of a given input string.
+
+    Tip1: A string permutation is any given rearrangement of its letters, for example boamtw is a permutation of "wombat".
+
+    solution("a")             == ["a"];
+    solution("ab").sort()     == ["ab", "ba"].sort();
+    solution("abc").sort()    == ["abc", "acb", "bac", "bca", "cab", "cba"].sort();
+    solution("wombat").length == 720;
+*/
+
+function solution1(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
