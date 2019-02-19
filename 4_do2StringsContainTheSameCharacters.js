d05d64afa9dd63ea8d6944a81bab8a4faac32cd0
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-19 16:45:10 -0500

    Added 4_do2StringsContainTheSameCharacters.js

---
+++ 4_do2StringsContainTheSameCharacters.js
@@ -0,0 +1,45 @@
+/*
+    Difficulty: Easy    
+
+    Write a function that accepts two String parameters, and returns true if they contain the same characters in any order taking into account letter case.
+
+    solution("abca",    "abca")  == true
+    solution("abc",     "cba")   == true
+    solution(" a1 b2 ", "b1 a2") == true
+    solution("abc",     "abca")  == true
+    solution("abc",     "Abc")   == false
+    solution("abc",     "cbAa")  == false
+*/
+
+// Loop
+function solution1(str1, str2){
+    
+}
+
+function solution2(str1, str2) {
+    
+}
+
+function solution3(str1, str2) {
+    
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge string for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+
+*/
