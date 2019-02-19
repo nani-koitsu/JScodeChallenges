4766fd0b2d32ca564f532996442ef8d64ab09c20
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-19 15:10:18 -0500

    Added 16_reverseTheWordsInAstring.js

---
+++ 16_reverseTheWordsInAstring.js
@@ -0,0 +1,25 @@
+/*
+    Difficulty: Taxing
+
+    Write a function that returns a string with each of its words reversed but in the original order, without using a loop.
+
+    solution("JavaScript Coding Challenges") == "tpircSavaJ gnidoC segnellahC";
+    solution("The quick brown fox")          == "ehT kciuq nworb xof";
+*/
+
+function solution(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// test solution()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+Performance of solution1() is 0.175 sec
+*/
