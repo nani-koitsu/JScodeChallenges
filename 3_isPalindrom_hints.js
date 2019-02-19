fcaaba8b5558600e7e77bae99988f5e2de03249f
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-19 16:19:46 -0500

    Added 3_isPalindrom_hints.js

---
+++ 3_isPalindrom_hints.js
@@ -0,0 +1,45 @@
+/*
+    Write a function that accepts a String as its only parameter, and returns true if the string reads the same when reversed, ignoring case.
+*/
+
+function isPalindrome1(str) {
+    // 1. make str lowercased
+    // 2. convert str to an array
+    // 3. reverse it
+    // 4. convert arr to a string and compare with an input
+}
+
+function isPalindrome2(str) {
+    // 1. make str lowercased
+    // 2. create variable to hold reversed string
+    // 3. loop through lowercased
+        // 4. attach lowercased[i] to the start of reversed string
+    // 5. compare lowercased with reversed
+}
+
+// provides earliest exit
+function isPalindrome3(str) {
+    // 1. make str lowercased
+    // 2. get string middle
+    // 3. loop through lowercased up to string middle
+        // 4. if (lowercased[i] != lowercased[lowercased.length - i - 1]) return false
+    // 5. return true
+}
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge string
+
+// test isPalindrome1()
+
+// test isPalindrome2()
+
+// test isPalindrome3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
