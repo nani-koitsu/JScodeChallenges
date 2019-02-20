76a28aa72ec0d31b83768e40fae7db66cd884066
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-20 14:54:21 -0500

    Added 18_generateArandomNumberInArange_hints.js

---
+++ 18_generateArandomNumberInArange_hints.js
@@ -0,0 +1,19 @@
+/*
+    Difficulty: Easy    
+
+    Write a function that accepts positive minimum and maximum integers, and returns a random number between those two bounds, inclusive.
+
+    solution(1, 5)  >= 1 && solution(1, 5)  <= 5
+    solution(8, 10) >= 8 && solution(8, 10) <= 10
+    solution(12, 12) == 12
+*/
+
+function solution1(min, max) {
+    // 1. if "min" is less than or equal to "max"
+        // 2. return "min"
+
+    // 3. use "Math" global object to generate random number
+    // 4. multiply random number by ("max" - "min") and add "min"
+
+    // 5. return result
+}
