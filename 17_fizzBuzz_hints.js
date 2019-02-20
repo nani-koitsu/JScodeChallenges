6f674e07a150d97e04d296f9b6744ac50ff34ab4
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-20 14:04:07 -0500

    Added 17_fizzBuzz_hints.js

---
+++ 17_fizzBuzz_hints.js
@@ -0,0 +1,39 @@
+/*
+    Difficulty: Easy    
+
+    Write a function that counts from 1 through "upTo", and prints "Fizz" if the counter is evenly divisible by 3, "Buzz" if it’s evenly divisible by 5, "Fizz Buzz" if it’s even divisible by three and five, or the counter number for all other cases.
+
+    solution(1).join("")  == [1].join("")
+    solution(2).join("")  == [1, 2].join("")
+    solution(3).join("")  == [1, 2, 'Fizz'].join("")
+    solution(4).join("")  == [1, 2, 'Fizz', 4].join("")
+    solution(15).join("") == [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz Buzz'].join("")
+*/
+
+function solution1(upTo) {
+    // 1. create "variable" result and set to an empty array
+
+    // 2. loop from 1 through "upTo"
+        // 3. if i % 3 and i % 5 are equal to 0
+            // 4. add "Fizz Buzz" to "results"
+        // 5. else if i % 5 equal to 0
+            // 6. add "Buzz" to "result"
+        // 7. else if i % 3 equal to 0
+            // 8. add "Fizz" to "result"
+        // 9 add i to "result"
+    
+    // 10. return result
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
