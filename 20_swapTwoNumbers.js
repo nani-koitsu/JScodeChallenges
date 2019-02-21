829bf86a2d105e3d97963c9a3d844c79d627f41c
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-21 13:40:24 -0500

    Added 20_swapTwoNumbers.js

---
+++ 20_swapTwoNumbers.js
@@ -0,0 +1,32 @@
+/*
+    Difficulty: Easy    
+
+    Swap two positive variable integers, a and b, without using a temporary variable.
+
+    Before running your code a should be 1 and b should be 2; afterwards, b should be 1 and a should be 2.
+    let a = 1;
+    let b = 2;
+    solution
+    a == 2 == true
+    b == 1 == true
+*/
+
+let a = 1;
+let b = 2;
+
+// solution1
+
+a = 1;
+b = 2;
+
+// solution2
+
+a = 1;
+b = 2;
+
+// solution3
+
+a = 1;
+b = 2;
+
+// solution4
