21d7cf61473b17857c2a0b5ad9755d6d0b669189
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-21 14:39:11 -0500

    Added 21_countingBinaryOnes.js

---
+++ 21_countingBinaryOnes.js
@@ -0,0 +1,36 @@
+/*
+    Difficulty: Tricky    
+
+    Create a function that accepts any positive integer and returns the next highest and next lowest number that has the same number of ones in its binary representation. If either number is not possible, return null for it.
+
+    Samples:
+    1) The number 12 is 1100 in binary, so it has two 1s. The next highest number with    that many 1s is 17, which is 10001. The next lowest is 10, which is 1010.
+       solution(12) == [10, 17]
+
+    2) The number 28 is 11100 in binary, so it has three 1s. The next highest number      with that many 1s is 35, which is 100011. The next lowest is 26, which is 11010.
+       solution(28) == [26, 35]
+*/
+
+
+function solution1(number) {
+}
+
+function solution2(number) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
