0f6b0f64f75a3bd1a086ce45e9f84ee7c447de59
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-21 16:40:16 -0500

    Added 6_countTheCharacters.js

---
+++ 6_countTheCharacters.js
@@ -0,0 +1,47 @@
+/*
+    Difficulty: Easy
+
+    Write a function that accepts a string, and returns how many times a specific character appears, taking case into account.
+
+    solution("The rain in Spain", "a")       == 2
+    solution("Mississippi", "i")             == 4
+    solution("Hacking with JavaScript", "i") == 3
+*/
+
+
+function solution1(str1, str2){
+}
+
+function solution2(str1, str2) {
+}
+
+function solution3(str1, str2) {
+}
+
+function solution4(str1, str2) {
+}
+
+function solution5(str1, str2) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge strings for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution3()
+
+// test solution4()
+
+// test solution5()
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
