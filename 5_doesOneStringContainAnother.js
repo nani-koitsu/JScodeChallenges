b38b494646e1a5dc5a18c11fcb4dcbe231f3137e
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-21 16:19:02 -0500

    Added 5_doesOneStringContainAnother.js

---
+++ 5_doesOneStringContainAnother.js
@@ -0,0 +1,44 @@
+/*
+    Difficulty: Easy
+
+    Write your own version of includes() method on String that ignores letter case, and without using existing includes() method.
+
+    solution("Hello, world", "Hello")   == true
+    solution("Hello, world", "WORLD")   == true
+    solution("Hello, world", "Goodbye") == false
+*/
+
+
+function solution1(str1, str2){
+}
+
+function solution2(str1, str2) {
+}
+
+function solution3(str1, str2) {
+}
+
+function solution4(str1, str2) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import huge string for performance tests
+
+// test solution1()
+
+// test solution2()
+
+// test solution3()
+
+// test solution4()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+
+*/
