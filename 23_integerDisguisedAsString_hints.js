bc2adf4004da8b3b8ba4c95da91d8ad62db3d0a8
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-25 14:56:55 -0500

    Added hints and stepBystep for challenge 23_integerDisguisedAsString

---
+++ 23_integerDisguisedAsString_hints.js
@@ -0,0 +1,40 @@
+/*
+    Difficulty: Tricky    
+
+    Write a function that accepts a string and returns true if it contains only numbers, i.e. the digits 0 through 9.
+
+    Samples:
+    solution("01010101")            == true
+    solution("123456789")           == true
+    solution("8353372039254775863") == true
+    solution("1.01")                == false
+
+    Hints:
+    1) use Set to leave only unique characters
+    2) create control string with all numbers
+    3) you can use parseInt() method to convert string to a number
+*/
+
+
+function solution1(str) {
+}
+
+function solution2(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
---
+++ 23_integerDisguisedAsString_sBs.js
@@ -0,0 +1,45 @@
+/*
+    Difficulty: Tricky    
+
+    Write a function that accepts a string and returns true if it contains only numbers, i.e. the digits 0 through 9.
+
+    Samples:
+    solution("01010101")            == true
+    solution("123456789")           == true
+    solution("8353372039254775863") == true
+    solution("1.01")                == false
+*/
+
+
+function solution1(str) {
+    // 1. create Set from "str" and save result to variable "set"
+    // 2. create string containing all numbers and save it to variable "numbers"
+
+    // 3. loop through "set"
+        // 4. if element of "set" is not in "numbers"
+            // 5. return "false"
+
+    // 6. return true
+}
+
+function solution2() {
+    // 1. convert "str" to number using "parseInt" method
+    // 2. compare number to "str" and return the result
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
