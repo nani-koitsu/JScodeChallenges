3ff7220439b41f2eeddf497193629e46f9498bfa
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-25 14:08:42 -0500

    Added hints and stepByStep description for challenge 22_binaryReverse

---
+++ 22_binaryReverse_hints.js
@@ -0,0 +1,37 @@
+/*
+    Difficulty: Tricky    
+
+    Create a function that accepts an unsigned 8-bit integer and returns its binary reverse, padded so that it holds precisely eight binary digits.
+
+    Tip: When you get the binary representation of a number, JavaScript will always use as few bits as possible – make sure you pad to eight binary digits before reversing.
+
+    Samples:
+    1) The number 32 is 100000 in binary, and padded to eight binary digits that’s 00100000. Reversing that binary sequence gives 00000100, which is 4. So, when given the input 32 your function should return 4.
+    solution(32) == 4
+
+    2) The number 41 is 101001 in binary, and padded to eight binary digits that 00101001. Reversing that binary sequence gives 10010100, which is 148. So, when given the input 41 your function should return 148.
+    solution(41) == 148
+
+    3) It should go without saying that your function should be  symmetrical: when fed 4 it should return 32, and when fed 148 it should return 41.
+    solution(4)   == 32
+    solution(148) == 41
+*/
+
+
+function solution1() {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
