b57d8ff64113cf84b67e77eb5b98fd43ee90e762
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-25 15:39:58 -0500

    Added hints and stepByStep instructions for challenge 24_AddNumbersInsideAString

---
+++ 24_AddNumbersInsideAString_hints.js
@@ -0,0 +1,39 @@
+/*
+    Difficulty: Tricky    
+
+    Given a string that contains both letters and numbers, write a function that pulls out all the numbers then returns their sum.
+
+    Samples:
+    solution("a1b2c3")    == 6
+    solution("a10b20c30") == 60
+    solution("cks8al")    == 8
+
+    Hints:
+    1) use control string containing all numbers
+    2) don't forget to convert string to number to calculate sum
+    3) use Regular Expression
+*/
+
+
+function solution1() {
+}
+
+function solution2() {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
---
+++ 24_AddNumbersInsideAString_sBs.js
@@ -0,0 +1,56 @@
+/*
+    Difficulty: Tricky    
+
+    Given a string that contains both letters and numbers, write a function that pulls out all the numbers then returns their sum.
+
+    Samples:
+    solution("a1b2c3")    == 6
+    solution("a10b20c30") == 60
+    solution("cks8al")    == 8
+*/
+
+
+function solution1() {
+    // 1. create control string containing all numbers and save it to variable "numbers"
+    // 2. create variable "number" equal to empty string
+    // 3. create varible "sum" equal to "0"
+
+    // 4. loop through str
+        // 5. if character is in "numbers"
+            // 6. add this character to "number"
+        // 7. else 
+            // 8. add "number" to "sum"
+            // 9. set "number" to be equal to empty string
+    
+    // 10. add "number" to "sum"
+
+    // 11. return sum
+}
+
+function solution2() {
+    // 1. create regular expression using "\\d+" pattern and global flag
+    // 2. save matches of regular expression to variable "matches"
+    // 3. create variable "sum" equal to "0"
+
+    // 4. loop through "matches"
+        // 5. add each match to "sum"
+
+    // 6. return "sum"
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
