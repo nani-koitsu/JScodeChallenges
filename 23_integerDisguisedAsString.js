8bfb4cd00ceddcb450f93aa2f3be822ca327f811
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-25 14:41:48 -0500

    Added 23_integerDisguisedAsString.js

---
+++ 23_integerDisguisedAsString.js
@@ -0,0 +1,35 @@
+/*
+    Difficulty: Tricky    
+
+    Write a function that accepts a string and returns true if it contains only numbers, i.e. the digits 0 through 9.
+
+    Samples:
+    solution("01010101")            == true
+    solution("123456789")           == true
+    solution("8353372039254775863") == true
+    solution("1.01")                == false
+*/
+
+
+function solution1(str) {
+}
+
+function solution2(str) {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
