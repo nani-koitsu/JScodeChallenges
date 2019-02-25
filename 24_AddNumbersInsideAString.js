2b7dbfffdb0a1d2860482e90a5611fb79fb0e61a
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-25 15:29:17 -0500

    Added 24_AddNumbersInsideAString.js

---
+++ 24_AddNumbersInsideAString.js
@@ -0,0 +1,34 @@
+/*
+    Difficulty: Tricky    
+
+    Given a string that contains both letters and numbers, write a function that pulls out all the numbers then returns their sum.
+
+    Samples:
+    solution("a1b2c3")    == 6
+    solution("a10b20c30") == 60
+    solution("cks8al")    == 8
+*/
+
+
+function solution1() {
+}
+
+function solution2() {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
