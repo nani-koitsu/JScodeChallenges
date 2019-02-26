b9f12d87600df42860fd8019916453148eb4a68a
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-26 13:53:36 -0500

    Added 25_calculateSquareRoot.js

---
+++ 25_calculateSquareRoot.js
@@ -0,0 +1,51 @@
+/*
+    Difficulty: Taxing    
+
+    Write a function that returns the square root of a positive integer, rounded down to the nearest integer, without using Math.sqrt().
+
+    Samples:
+    solution(9)        == 3
+    solution(16777216) == 4096
+    solution(16)       == 4
+    solution(15)       == 3
+*/
+
+
+function solution1() {
+}
+
+
+function solution2() {
+}
+
+
+function solution3() {
+}
+
+
+function solution4() {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+
+// test solution2()
+
+
+// test solution3()
+
+
+// test solution4()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
