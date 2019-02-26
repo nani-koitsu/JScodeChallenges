fd323a1a7fa001ec2d312330f0af1e9bf160b2cd
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-26 15:07:27 -0500

    Added 26_subtractWithoutSubtract.js

---
+++ 26_subtractWithoutSubtract.js
@@ -0,0 +1,42 @@
+/*
+    Difficulty: Taxing    
+
+    Write a function that subtract one positive integer from another, without using "-".
+
+    Samples:
+    solution(5, 9)   == 4
+    solution(10, 30) == 20
+*/
+
+
+function solution1() {
+}
+
+
+function solution2() {
+}
+
+
+function solution3() {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+
+// test solution2()
+
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
