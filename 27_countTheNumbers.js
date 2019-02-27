fd26ab43599ddf67a7a5b2baec28f1e95744b189
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-27 13:56:10 -0500

    Added challenge 27_countTheNumbers

---
+++ 27_countTheNumbers.js
@@ -0,0 +1,42 @@
+/*
+    Difficulty: Easy    
+
+    Write an extension for array of integers that returns the number of times a specific digit appears in any of its numbers.
+
+    Samples:
+    [5, 15, 55, 515].solution("5") == 6
+    [5, 15, 55, 515].solution("1") == 2
+    [55555].solution("5")          == 5
+    [55555].solution("1")          == 0
+*/
+
+
+// solution1()
+
+
+// solution2()
+
+
+// solution3()
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// test solution1()
+
+
+// test solution2()
+
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+
+*/
