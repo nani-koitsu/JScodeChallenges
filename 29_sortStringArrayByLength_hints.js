bc3f49b321d3acfcbad959df6b8085ed0a9cfec1
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-27 15:44:16 -0500

    Added hints and stepByStep description for challenge 29_sortStringArrayByLength

---
+++ 29_sortStringArrayByLength_hints.js
@@ -0,0 +1,32 @@
+/*
+    Difficulty: Easy    
+
+    Extend arrays with a function that returns an array of strings sorted by their lengths, longest first.
+
+    Samples:
+    ["a", "abc", "ab"].solution()                      == ["abc", "ab", "a"]
+    ["Saki", "Q", "Joe" ,"Chris", "Jordan"].solution() == ["Jordan", "Chris", "Saki", "Joe", "Q"]
+    [].solution() == []
+
+    Hints:
+    1) By defoult "sort" method sorts elements alphabetically. To sort numerically subtract 2nd element from 1st.
+*/
+
+
+// solution1
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big string for test
+
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
---
+++ 29_sortStringArrayByLength_sBs.js
@@ -0,0 +1,31 @@
+/*
+    Difficulty: Easy    
+
+    Extend arrays with a function that returns an array of strings sorted by their lengths, longest first.
+
+    Samples:
+    ["a", "abc", "ab"].solution()                      == ["abc", "ab", "a"]
+    ["Saki", "Q", "Joe" ,"Chris", "Jordan"].solution() == ["Jordan", "Chris", "Saki", "Joe", "Q"]
+    [].solution() == []
+*/
+
+
+// solution1
+// 1. add method "solution1" to array prototype
+// 2. sort "this" by subtracting length of 1st element from length of 2nd element and return it
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big string for test
+
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
