af7ac0077b6276ddc47aeff98451967b858b0a71
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-27 15:02:11 -0500

    Added hints and stepByStep description for challenge 28_findNSmallest

---
+++ 28_findNSmallest_sBs.js
@@ -0,0 +1,41 @@
+/*
+    Difficulty: Easy    
+
+    Write an extension for array that returns the N smallest elements as an array, sorted smallest first, where N is an integer parameter.
+
+    Samples:
+    [1, 2, 3, 1002, 4].solution(3) == [1, 2, 3]
+    ["q", "f", "k"].solution(10)   == ["f", "k", "q"]
+    [250, 15].solution(3)          == [15, 250]
+    [].solution(3)                 == []
+*/
+
+
+Array.prototype.solution1 = function(count) {
+    // 1. check if array is not empty
+
+    // 2. if type of first element is "number"
+        // 3. sort array using condition "e1 - e2"
+    // 4. else
+        // 5. sort array as usually
+
+    // 6. return part of array up to "count"
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// import big string for test
+
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
