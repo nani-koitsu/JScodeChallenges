43e097982383b848b4d8c6d1a2e92e6ada8c264a
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-27 15:41:36 -0500

    Added challenge 29_sortStringArrayByLength

---
+++ 29_sortStringArrayByLength.js
@@ -0,0 +1,29 @@
+/*
+    Difficulty: Easy    
+
+    Extend arrays with a function that returns an array of strings sorted by their lengths, longest first.
+
+    Samples:
+    ["a", "abc", "ab"].solution()                      == ["abc", "ab", "a"]
+    ["Saki", "Q", "Joe" ,"Chris", "Jordan"].solution() == ["Jordan", "Chris", "Saki", "Joe", "Q"]
+    [].solution() == []
+*/
+
+
+// solution1
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big string for test
+
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
