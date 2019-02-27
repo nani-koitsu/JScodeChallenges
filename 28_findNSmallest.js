961d0eb13162c100a649daea769632d48d65e55e
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-27 14:47:33 -0500

    Added challenge 28_findNSmallest

---
+++ 28_findNSmallest.js
@@ -0,0 +1,33 @@
+/*
+    Difficulty: Easy    
+
+    Write an extension for array that returns the N smallest elements as an array, sorted smallest first, where N is an integer parameter.
+
+    Samples:
+    [1, 2, 3, 1002, 4].solution(3) == [1, 2, 3]
+    ["q", "f", "k"].solution(10)   == ["f", "k", "q"]
+    [250, 15].solution(3)          == [15, 250]
+    [].solution(3)                 == []
+*/
+
+
+Array.prototype.solution1 = function() {
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import numbers for test
+
+// import big string for test
+
+
+// test solution1()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
