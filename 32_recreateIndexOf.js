1f949e5985d9216e89874951e5461a76e737f547
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-28 15:26:25 -0500

    Added challenge 32_recreateIndexOf.js

---
+++ 32_recreateIndexOf.js
@@ -0,0 +1,36 @@
+/*
+    Difficulty: Easy    
+
+    Write an extension for array that reimplements the indexOf method.
+
+    Tip: This is one of the easiest standard library methods to reimplement, so please give it an especially good try before reading any hints.
+
+    Samples:
+    [1, 2, 3].solution(1) == 0
+    [1, 2, 3].solution(3) == 2
+    [1, 2, 3].solution(5) == null
+*/
+
+// solution1
+
+
+// solution2
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+
+// test solution1()
+
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
