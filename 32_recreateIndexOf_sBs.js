1ec4b78d4f4eca31c26a389698fe8dfd321f6f89
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-28 15:31:15 -0500

    Added hints and stepByStep for challenge 32_recreateIndexOf

---
+++ 32_recreateIndexOf_sBs.js
@@ -0,0 +1,50 @@
+/*
+    Difficulty: Easy    
+
+    Write an extension for array that reimplements the indexOf method.
+
+    Tip: This is one of the easiest standard library methods to reimplement, so please give it an especially good try before reading any hints.
+
+    Samples:
+    [1, 2, 3].solution(1) == 0
+    [1, 2, 3].solution(3) == 2
+    [1, 2, 3].solution(5) == null
+*/
+
+// solution1
+Array.prototype.solution1 = function(element) {
+    // 1. loop through array 
+        // 2. "array[index]" is equal to "element"
+            // 3. return "index"
+
+    // 4. return "null"
+}
+
+
+// solution2
+Array.prototype.solution2 = function(element) {
+    // 1. check if "element" exists in array
+
+    // 2. loop through array 
+        // 3. "array[index]" is equal to "element"
+            // 4. return "index"
+}
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+
+// test solution1()
+
+
+// test solution2()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
