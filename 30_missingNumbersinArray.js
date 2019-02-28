d6a7a00f7fb288629ba061e6ca233814e02f6762
Yury Shkoda <shkoda.yuri@gmail.com>
2019-02-28 13:51:22 -0500

    Added challenge 30_missingNumbersinArray

---
+++ 30_missingNumbersinArray.js
@@ -0,0 +1,42 @@
+/*
+    Difficulty: Easy    
+
+    Extend arrays with a function that accepts an array of numbers from 0 to 100 where zero or more numbers might be missing, and returns an array of the missing numbers.
+
+    Samples:
+    let arr = [...Array(101).keys()];
+    arr.splice(35, 1);
+    arr.splice(20, 1);
+    arr.splice(7, 1);
+    arr.solution() == [8, 21, 36];
+*/
+
+// solution1
+
+
+// solution2
+
+
+// solution3
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+
+// test solution1()
+
+
+// test solution2()
+
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
