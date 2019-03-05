cd1a3db797ddca7cb1705b8ed3450a71d2b96c33
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-05 14:00:11 -0500

    Added challenge 33_linkedLists

---
+++ 33_linkedLists.js
@@ -0,0 +1,13 @@
+/*
+    Difficulty: Easy    
+
+    Create a linked list of lowercase English alphabet letters, along with a method that traverses all nodes and prints their letters on a single line separated by spaces.
+
+    Tip: 
+    1) This is several problems in one. First, create a linked list data structure, which itself is really two things. Second, use your linked list to create the alphabet. Third, write a method traverses all nodes and prints their letters.
+    2) You should use a class for this. Yes, really.
+    3) Once you complete your solution, keep a copy of the code – you’ll need it for future challenges!
+
+    Samples:
+    list.printNodes() == "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+*/
