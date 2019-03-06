3ecf49b8796e30514b84864185168071a004562c
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-06 14:56:29 -0500

    Added challenge 35_recreateMap()

---
+++ 35_recreateMap().js
@@ -0,0 +1,31 @@
+/*
+    Difficulty: Tricky    
+
+    Write an extension for array that reimplements the "map()" method.
+
+    Samples:
+    [1, 2, 3].solution(e => `${e}`)                == ['1', '2', '3']
+    ['1', '2', '3'].solution(e => parseInt(e))     == [1, 2, 3]
+    [8, 3, 6].solution((e, i) => `${i}: ${e * 2}`) == ['0: 16', '1: 6', '2: 12']
+*/
+
+// solution1
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+
+// test solution1()
+
+
+// test default map()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
