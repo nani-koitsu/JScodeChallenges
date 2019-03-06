a26f989531b4979b81037ba976f30b03f9ab3ee1
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-06 14:06:44 -0500

    added challenge 34_linkedListMidPoint

---
+++ 34_linkedListMidPoint.js
@@ -0,0 +1,16 @@
+/*
+    Difficulty: Easy    
+
+    Create a linked list of lowercase English alphabet letters, along with a method that traverses all nodes and prints their letters on a single line separated by spaces.
+
+    Tip: 
+    1) If linked list contains an even number of items, returning the one before or one after the center is acceptable.
+
+    Samples:
+    1) if linked list contains "1, 2, 3, 4, 5"
+    list.centerNode() == 3;
+    2) if linked list contains "1, 2, 3, 4"
+    list.centerNode() == 3 || list.centerNode() == 2;
+    3) if linked list contains English alphabet
+    list.centerNode() == m || list.centerNode() == "n";
+*/
