a207dea585e411bd1e1ccfb4ab6fcf1d63b36ea0
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-07 14:52:42 -0500

    Added challenge 37_implementDeque

---
+++ 37_implementDeque.js
@@ -0,0 +1,19 @@
+/*
+    Difficulty: Tricky    
+
+    Create a new data type that models a double-ended queue, or deque. You should be able to push items to the front or back, pop them from the front or back, and get the number of items.
+
+    Samples:
+    *Your code*
+    let numbers = new Deque();
+    numbers.pushBack(5);
+    numbers.pushBack(8);
+    numbers.pushFront(7);
+    numbers.pushBack(3);
+    numbers.count    == 4
+    numbers.popFront == 7
+    numbers.popBack  == 3
+    numbers.popFront == 7
+    numbers.popFront == 8
+    numbers.popBack  == null
+*/
