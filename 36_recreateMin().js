638a1044896e4c6b1fc1ae2f48678d9ae4c04f09
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-07 13:49:40 -0500

    Added challenge 36_recreateMin()

---
+++ 36_recreateMin().js
@@ -0,0 +1,43 @@
+/*
+    Difficulty: Tricky    
+
+    Write an extension for array that reimplements the "Math.min()" method.
+
+    Samples:
+    [1, 2, 3].solution()       == Math.min(...[1, 2, 3])
+    ["q", "f", "k"].solution() == "f"
+    [4096, 256, 16].solution() == 16
+    [].solution()              == null
+*/
+
+// solution1
+
+
+// solution2
+
+
+// solution3
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+// import big string for test
+
+
+// test solution1()
+
+
+// test solution1()
+
+
+// test solution3()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
