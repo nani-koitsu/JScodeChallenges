b7316815a3710cb71255f85f819acd65471fa794
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-07 10:21:35 -0500

    Added Polyfill solution to challenge 35_recreateMap()

--- 35_recreateMap()_solved.js
+++ 35_recreateMap()_solved.js
@@ -23,6 +23,42 @@ console.assert([1, 2, 3].solution(e => `${e}`).join("")                == ['1', 
 console.assert(['1', '2', '3'].solution(e => parseInt(e)).join("")     == [1, 2, 3].join(""), `assertion2 failed!`);
 console.assert([8, 3, 6].solution((e, i) => `${i}: ${e * 2}`).join("") == ['0: 16', '1: 6', '2: 12'].join(""), `assertion3 failed!`);
 
+// Polyfill https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map#Polyfill
+Array.prototype.polyfill = function(callback) {
+    var T, A, k;
+
+    if (this == null) {
+        throw new TypeError('this is null or not defined');
+    }
+
+    var O   = Object(this);
+    var len = O.length >>> 0;
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+    }
+
+    if (arguments.length > 1) {
+        T = arguments[1];
+    }
+
+    A = new Array(len);
+    k = 0;
+
+    while (k < len) {
+        var kValue, mappedValue;
+    
+        if (k in O) {
+            mappedValue = callback.call(T, kValue, k, O); 
+            A[k] = mappedValue;
+        } 
+        
+        k++;
+    }
+    
+    return A;
+};
+
 
 /*
 ************************* PERFORMANCE TESTS *************************
@@ -60,9 +96,21 @@ performance = (finishTime - startTime) / 1000;
 
 console.log(`Performance of default map() is ${ performance } sec`);
 
+// test polyfill()
+startTime = new Date().getTime();
+
+testNum.polyfill((e, i) => `${i}: ${e}`);
+text1.polyfill((e, i) => `${i}: ${e}`);
+
+finishTime  = new Date().getTime();
+performance = (finishTime - startTime) / 1000;
+
+console.log(`Performance of polyfill() is ${ performance } sec`);
+
 /*
 ************************* PERFORMANCE RESULTS *************************
 
-Performance of solution()    is 0.373 sec
-Performance of default map() is 0.384 sec
+Performance of solution()    is 0.375 sec
+Performance of default map() is 0.4 sec
+Performance of polyfill()    is 0.231 sec
 */
