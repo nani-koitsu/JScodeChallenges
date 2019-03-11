c4278a33af79be4f5c300e83ed4a6832e8af9b1e
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-11 13:54:10 -0400

    Added challenge 38_sumTheEvenRepeats

---
+++ 38_sumTheEvenRepeats.js
@@ -0,0 +1,33 @@
+/*
+    Difficulty: Tricky    
+
+    Write an extension for array that works with a variadic array of integers and return the sum of all numbers that appear an even number of times.
+
+    Samples:
+    solution([1, 2, 2, 3, 3, 4])       == 5
+    solution([5, 5, 5, 12, 12])        == 12
+    solution([1, 1, 2, 2, 3, 3, 4, 4]) == 10
+*/
+
+// solution1
+
+
+// solution2
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+// duplicate some numbers
+
+// test solution1()
+
+// test solution2()
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
