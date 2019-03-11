cd2049e22e85ce4abe9f88674086c6f5f5945135
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-11 14:07:04 -0400

    Added hints and stepByStep description for challenge 38_sumTheEvenRepeats

---
+++ 38_sumTheEvenRepeats_hints.js
@@ -0,0 +1,37 @@
+/*
+    Difficulty: Tricky    
+
+    Write an extension for array that works with a variadic array of integers and return the sum of all numbers that appear an even number of times.
+
+    Samples:
+    solution([1, 2, 2, 3, 3, 4])       == 5
+    solution([5, 5, 5, 12, 12])        == 12
+    solution([1, 1, 2, 2, 3, 3, 4, 4]) == 10
+    
+    Hints:
+    1) filter array to remain searched item and check filtered length
+    2) try to use nested function to improve performance
+*/
+
+// solution1
+
+
+// solution2
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+// duplicate some numbers
+
+// test solution1()
+
+// test solution2()
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
---
+++ 38_sumTheEvenRepeats_sBs.js
@@ -0,0 +1,51 @@
+/*
+    Difficulty: Tricky    
+
+    Write an extension for array that works with a variadic array of integers and return the sum of all numbers that appear an even number of times.
+
+    Samples:
+    solution([1, 2, 2, 3, 3, 4])       == 5
+    solution([5, 5, 5, 12, 12])        == 12
+    solution([1, 1, 2, 2, 3, 3, 4, 4]) == 10
+*/
+
+// solution1
+    // 1. create variable "sum" equel to "0"
+    // 2. create "set" from initial array
+
+    // 3. loop through set
+        // 4. filter initial array to remain only particular item
+        // 5. if filtered length is even
+            // 6. add item to "sum"
+
+    // 7. return "sum"
+
+
+// solution2
+    // 1. create variable "sum" equel to "0"
+    // 2. create "set" from initial array
+
+    // 3. write function "appearEvenTimes" that will return true or false
+
+    // 4. loop through set
+        // 5. add item to "sum" if it appeared even times
+
+    // 6. return "sum"
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+// duplicate some numbers
+
+// test solution1()
+
+// test solution2()
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
