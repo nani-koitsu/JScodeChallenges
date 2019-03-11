d494a79cbb9faf55c7e4a81f0bc17192310eb7fd
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-11 15:13:01 -0400

    Added challenge 39_countTheLargestRange

---
+++ 39_countTheLargestRange.js
@@ -0,0 +1,34 @@
+/*
+    Difficulty: Tricky    
+
+    Write an extension for array of positive and negative numbers and returns a closed range containing the position of the contiguous positive numbers that sum to the highest value, or nil if nothing were found.
+
+    Samples:
+    [0, 1, 1, -1, 2, 3, 1].solution()            == [4, 5, 6]
+    [10, 20, 30, -10, -20, 10, 20].solution() == [0, 1, 2]
+    [1, -1, 2, -1].solution()                 == [2]
+    [2, 0, 2, 0, 2].solution()                == [0]
+    [].solution()                             == null
+
+    Hints:
+    1) Use loop to track best range
+*/
+
+// solution
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// import big numbers for test
+
+// add negative numbers to test array
+
+// test solution()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
