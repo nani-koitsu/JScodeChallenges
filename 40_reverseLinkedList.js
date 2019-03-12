25320089bd32e23cfd9a02dec66c119548c3a2d8
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-12 13:58:12 -0400

    Added challenge 40_reverseLinkedList

---
+++ 40_reverseLinkedList.js
@@ -0,0 +1,13 @@
+/*
+    Difficulty: Tricky    
+
+    Expand your code from challenge 33 so that it has "reversed()" method that returns a copy of itself in reverse.
+
+    Tip: Don't cheat! Don't reverse the alphabet letters before creating linked list!
+
+    Samples:
+    1) let listReversed = list.reversed();
+       listReversed.printNodes() == "z y x w v u t s r q p o n m l k j i h g f e d c b a"
+*/
+
+// solution
