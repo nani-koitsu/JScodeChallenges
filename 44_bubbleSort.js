5b746bb773d7ada91978f583308069d4f964c32c
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-14 14:35:44 -0400

    Added challenge 44_bubbleSort

---
+++ 44_bubbleSort.js
@@ -0,0 +1,38 @@
+/*
+    Difficulty: Easy    
+
+    Create an extension for arrays that sort them using bubble sort.
+
+    Tip: A bubble sort repeatedly loops over the items in an array, comparing items that are next to each other and swapping them if they aren’t sorted. This looping continues until all items are in their correct order.
+
+    Samples:
+    1) [12, 5, 4, 9, 3, 2, 1].solution() == [1, 2, 3, 4, 5, 9, 12]
+    2) ["f", "a", "b"].solution() == ["a", "b", "f"]
+    2) [].solution() == []
+*/
+
+// solution1
+
+
+// solution2
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// get big numbers for test
+
+// shuffle numbers
+
+// test solution1
+
+// test solution2
+
+// test dafault sort()
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
