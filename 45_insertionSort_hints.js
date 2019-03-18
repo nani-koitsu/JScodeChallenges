e5d6cad8c51f468e0626d6cd7f4e0e8e42631b0b
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-18 14:17:45 -0400

    Added hints for challenge 45_insertionSort

---
+++ 45_insertionSort_hints.js
@@ -0,0 +1,40 @@
+/*
+    Difficulty: Easy    
+
+    Create an extension for arrays that sort them using the insertion sort algorithm.
+
+    Tip: An insertion sort creates a new, sorted array by removing items individually from the input array and placing them into the correct position in the new array;
+
+    Samples:
+    1) [12, 5, 4, 9, 3, 2, 1].solution() == [1, 2, 3, 4, 5, 9, 12]
+    2) ["f", "a", "b"].solution() == ["a", "b", "f"]
+    2) [].solution() == []
+
+    Hints:
+    1) In the most simple solution, you should be able to pick out an item from your source array, then search through your sorted destination array to find where it should go.
+    2) Advanced solution would be to pull out the current item you want to sort, then keep moving other elements to the right until you find the correct spot for your item. 
+*/
+
+// solution1
+    
+
+// solution2
+    
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// get big numbers for test
+
+// shuffle numbers
+
+// test solution1
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
