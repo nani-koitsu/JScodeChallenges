ce733ce343fb1fa3624cd40d8cd59e57eae3102e
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-18 15:10:04 -0400

    Added challenge 46_isomorphicValues

---
+++ 46_isomorphicValues.js
@@ -0,0 +1,43 @@
+/*
+    Difficulty: Easy    
+
+    Write a function that accepts two values and returns true if they are isomorphic. That is, each part of the value must map to precisely one other, but that might be itself.
+
+    Tip: Strings A and B are considered isomorphic if you can replace all instances of each letter with another. For example, “tort” and “pump” are isomorphic, because you can replace both Ts with a P, the O with a U, and the R with an M. For integers you compare individual digits, so 1231 and 4564 are isomorphic numbers. For arrays you compare elements, so [1, 2, 1] and [4, 8, 4] are isomorphic.
+
+    Samples:
+    1)  solution("clap", "slap")                   == true
+    2)  solution("rum", "mud")                     == true
+    3)  solution("pip", "did")                     == true
+    4)  solution("carry", "baddy")                 == true
+    5)  solution("cream", "lapse")                 == true
+    6)  solution("123123", "456456")               == true
+    7)  solution("3.14159", "2.03048")             == true
+    8)  solution([1, 2, 1, 2, 3], [4, 5, 4, 5, 6]) == true    
+    9)  solution("carry", "daddy")                 == false
+    10) solution("did", "cad")                     == false
+    11) solution("maim", "same")                   == false
+    12) solution("curry", "flurry")                == false
+    13) solution("112233", "112211")               == false
+*/
+
+// solution1
+
+// solution2 (cheat)
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// get big string for test
+
+// test solution1
+
+// test solution2
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
