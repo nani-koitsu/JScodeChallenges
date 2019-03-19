823e79f69d32b1ca988cdacb4cc4c7d3624bf440
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-19 14:16:55 -0400

    Added hints for challenge 47_balancedBrackets

---
+++ 47_balancedBrackets_hints.js
@@ -0,0 +1,38 @@
+/*
+    Difficulty: Easy    
+
+    Write a function that accepts a string containing the characters "(", "[", "{", "<", ">", "}", "]", and ")" in any arrangement and frequency. It should return true if the brackets are opened and closed in the correct order, and if all brackets are closed. Any other input should false.
+
+    Samples:
+    1)  solution("()")           == true
+    2)  solution("([])")         == true
+    3)  solution("([])(<{}>)")   == true
+    4)  solution("([]{}<[{}]>)") == true
+    5)  solution("")             == true
+    6)  solution("}{")           == false
+    7)  solution("([)]")         == false
+    8)  solution("([)")          == false
+    9)  solution("([")           == false
+    11) solution("[<<<{}>>]")    == false
+    12) solution("Hello")        == false
+
+    Hints:
+    1) use cobject to map opening and closing brackets
+    2) use queue to track sequence
+*/
+
+// solution
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// get big string for test
+
+// test solution
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
