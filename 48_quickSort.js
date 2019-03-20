4809ccfe8a73b7b021916bda0fee73d43b064117
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-20 13:57:29 -0400

    Added challenge 48_quickSort

---
+++ 48_quickSort.js
@@ -0,0 +1,35 @@
+/*
+    Difficulty: Tricky    
+
+    Create an extension for arrays that sorts them using the quicksort algorithm.
+
+    Tip: 
+    The quicksort algorithm picks an item from its array to use as the pivot point, then splits itself into either two parts (less than or greater than) or three (less, greater, or equal). These parts then repeat the pivot and split until the entire array has been split, then it gets rejoined so that less, equal, and greater are in order.
+
+    Samples:
+    1) [12, 5, 4, 9, 3, 2, 1].solution() == [1, 2, 3, 4, 5, 9, 12]
+    2) ["f", "a", "b"].solution() == ["a", "b", "f"]
+    3) [].solution() == []
+*/
+
+// solution1
+
+// solution2
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+// get big numbers for test
+
+// shuffle numbers
+
+// test solution1
+
+// test solution2
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
