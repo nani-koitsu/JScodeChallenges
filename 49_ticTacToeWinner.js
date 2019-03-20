736835084a4a182b5accbfe03b3eecaf1e4f80be
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-20 15:08:29 -0400

    Added challenge 49_ticTacToeWinner

---
+++ 49_ticTacToeWinner.js
@@ -0,0 +1,21 @@
+/*
+    Difficulty: Tricky    
+
+    Create array extension that detects whether either player has won in a game of Tic-Tac-Toe.
+
+    Tip: A tic-tac-toe board is 3x3, containing single letters that are either X, O, or empty. A win is three Xs or Os in a straight line.
+
+    Samples:
+    1) [["X", "",  "O"], 
+        ["",  "X", "O"], 
+        ["",  "",  "X"]].solution() == true
+    2) [["O", "",  "X"], 
+        ["O", "",  "X"], 
+        ["O", "",  ""]].solution() == true
+    3) [["",  "X", ""], 
+        ["O", "X", ""], 
+        ["O", "",  "X"]].solution() == false
+    4) [["",  "",  ""], 
+        ["",  "",  ""], 
+        ["",  "",  ""]].solution() == false
+*/
