7e93bc189f0858e3c2b333f560b21a7c8ce3c2cd
Yury Shkoda <shkoda.yuri@gmail.com>
2019-03-21 10:46:33 -0400

    Added  challenge 50_nQueens

---
+++ 50_nQueens.js
@@ -0,0 +1,41 @@
+/*
+    Difficulty: Taxing    
+
+    There are "M" different ways you can place "N" queens on an "NxN" chessboard so that none of them are able to capture others. Write a function that calculates them all and prints them to the screen as a visual board layout, and returns the number of solutions it found.
+
+    Tip: A queen moves in straight lines vertically, horizontally, or diagonally. You need to place all eight queens so that no two share the same row, column, or diagonal.
+
+    Samples:
+    1) In an 8x8 board you need to place 8 queens. There are 92 possible arrangements, so your function should print each of them then return 92.
+    let board  = new Array(8)
+    let queens = 0
+    solution(board, queens) == 92
+    2) In a 10x10 board you need to place 10 queens. There are 724 possible arrangements, so your function should print each of them then return 724.
+    board  = new Array(10)
+    queens = 0
+    solution(board, queens) == 724
+
+    Here is a suggested example layout for solutions:
+    .......Q
+    ...Q....
+    Q.......
+    ..Q.....
+    .....Q..
+    .Q......
+    ......Q.
+    ....Q...
+*/
+
+// solution
+
+
+
+/*
+************************* PERFORMANCE TESTS *************************
+*/
+
+
+/*
+************************* PERFORMANCE RESULTS *************************
+
+*/
